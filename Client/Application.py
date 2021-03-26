4cc8d37a8ca5d2feee255542c53caf9782542888
Aleksandr Torba <o.torba@plarium.com>
2021-03-26 18:13:33 +0200

    Client: init start of client app

--- Client/Application.py
+++ Client/Application.py
@@ -1,3 +1,16 @@
+from PyQt5 import QtWidgets
+import sys
+
+from Client.ui_main_window import Ui_MainWindow
+
+
 class Application:
     def start(self):
-        pass
+        app = QtWidgets.QApplication(sys.argv)
+        window = QtWidgets.QMainWindow()
+
+        ui = Ui_MainWindow()
+        ui.setupUi(window)
+
+        window.show()
+        sys.exit(app.exec())
